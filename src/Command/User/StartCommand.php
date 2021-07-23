0ae47fbd4128fac43e6fffda2278a0e137b11758
ElbekU <87022363+ElbekU@users.noreply.github.com>
2021-07-23 05:11:53 -0700

    Update StartCommand.php

--- src/Command/User/StartCommand.php
+++ src/Command/User/StartCommand.php
@@ -57,8 +57,8 @@ class StartCommand extends UserCommand
         }
 
         $text = Emoji::wavingHand() . ' ';
-        $text .= '<b>' . __('Hi!') . '</b>' . PHP_EOL;
-        $text .= __('To begin, start a message with {USAGE} in any of your chats or click the {BUTTON} button and then select a chat to play in.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
+        $text .= '<b>' . __('Salom men @dCompiler ning botiman!') . '</b>' . PHP_EOL;
+        $text .= __('Boshlash uchun {USAGE} va o\'yin nomini kiritib  {BUTTON} tugmani bosing.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
 
         $data = [
             'chat_id'                  => $chat_id,
