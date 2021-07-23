a1613c571a66e78201824559e60c78cf1d7171a8
ElbekU <87022363+ElbekU@users.noreply.github.com>
2021-07-23 09:21:11 -0700

    Update StatsCommand.php

--- src/Command/Admin/StatsCommand.php
+++ src/Command/Admin/StatsCommand.php
@@ -29,7 +29,7 @@ class StatsCommand extends AdminCommand
 {
     protected $name = 'stats';
     protected $description = 'Display stats';
-    protected $usage = '/stats';
+    protected $usage = '/statistika';
 
     /**
      * @return ServerResponse
